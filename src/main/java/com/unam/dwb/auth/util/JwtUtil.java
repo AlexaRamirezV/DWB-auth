e2d3ea7e9169b56259b0a1a69115c8a9b4ed9cd3
Alexa Ramírez <alexa.ramirezv@ciencias.unam.mx>
2025-12-02 13:08:50 -0600

    Implementacion de auth

---
+++ src/main/java/com/unam/dwb/auth/util/JwtUtil.java
@@ -0,0 +1,81 @@
+package com.unam.dwb.auth.util;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.unam.dwb.auth.domain.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "8J+YjvCfpJPwn5ic8J+YmvCfmI3wn6Ww8J+ZgvCfpKM="; 
+    private static final SecretKey secretKey = new SecretKeySpec(Base64.getDecoder().decode(SECRET_KEY), "HmacSHA256");
+    
+    
+    @Value("${system.hostname}")
+    private String hostname;
+
+    public String generateToken(Usuario usuario) {
+        Instant now = Instant.now();
+        Duration tokenValidity = Duration.ofHours(9);
+    	
+        return Jwts.builder()
+        		.claim("id", usuario.getId())
+        		.claim("email", usuario.getCorreo())
+        		.claim("roles", usuario.getAuthorities())
+        		.setIssuer("http://" + hostname)
+        		.setSubject(usuario.getUsername())
+                .setAudience("http://" + hostname)
+                .setExpiration(Date.from(now.plus(tokenValidity)))
+                .setIssuedAt(new Date())
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+    	
+    	JwtParser jwtParser = Jwts.parserBuilder()
+    			.setSigningKey(secretKey)
+    			.build();
+    	
+    	return jwtParser.parseClaimsJws(token).getBody();
+    	
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+	@SuppressWarnings("unchecked")
+	public List<HashMap<String, String>> extractPermisos(String token) {
+         return extractClaims(token).get("roles", List.class);
+    }
+
+    public boolean isTokenValid(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(extractClaims(token));
+    }
+}
